d3a7b81a7fffdca18bec3c893c594ee963360a34
mbattaloglu <mmbattaloglu@gmail.com>
2024-02-27 01:27:20 +0300

    files

---
+++ src/components/PackingList.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Item } from "./Item";
+
+export function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearItems,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  switch (sortBy) {
+    case "input":
+      sortedItems = items;
+      break;
+    case "description":
+      sortedItems = items
+        .slice()
+        .sort((a, b) => a.description.localeCompare(b.description));
+      break;
+    case "packed":
+      sortedItems = items
+        .slice()
+        .sort((a, b) => Number(a.packed) - Number(b.packed));
+      break;
+    default:
+      sortedItems = items;
+      break;
+  }
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((i) => (
+          <Item
+            item={i}
+            key={i.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select
+          value={sortBy}
+          onChange={(e) => {
+            setSortBy(e.target.value);
+          }}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearItems}>Clear List</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Stats.js
@@ -0,0 +1,26 @@
+export function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((i) => i.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go to the ✈️"
+          : `
+        👜 You have ${numItems} items on your list and you already packed
+        ${numPacked} (${percentage}%)
+        `}
+      </em>
+    </footer>
+  );
+}
